207558e5a601bdfada60b1bc7e2d2b502e200603
Lucdomingues <lojalucc@outlook.com>
2022-07-13 23:02:53 -0300

    requisitos obrigatorios

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const btnEnviar = document.getElementById('btn-enviar');
-const email = document.querySelector('#email');
-const senha = document.querySelector('#password');
-
-const texto = email.value;
-const text = senha.value;
-
-console.log(texto);
-function enviar() {
-  if (texto === 'tryber@teste.com' && text === 123456) {
+const chex = document.getElementById('agreement');
+document.getElementById("submit-btn").disabled = true;
+ 
+function enviar(event) {
+  event.preventDefault()
+  const email = document.querySelector('#email').value;
+  const senha = document.querySelector('#password').value;
+  if (email === 'tryber@teste.com' && senha === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
+  console.log(senha);
+  console.log(email);
 }
 
+function habilitar() {
+  if (chex.checked === true) {
+    document.getElementById("submit-btn").disabled = false;
+  } else {
+    document.getElementById("submit-btn").disabled = true;
+  }
+}
+
+chex.addEventListener('click', habilitar);
 btnEnviar.addEventListener('click', enviar);
