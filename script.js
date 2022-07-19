12fcba62574809c4df0477511e6b7431208f7420
Leonardo Selegar <leonardo11132@gmail.com>
2022-07-18 23:05:07 -0300

    Metade do requisito 21

--- script.js
+++ script.js
@@ -2,7 +2,29 @@ const btnEnviar = document.getElementById('btn-enviar');
 const chex = document.getElementById('agreement');
 const botao = document.getElementById('submit-btn');
 const text = document.getElementById('textarea');
+const form1 = document.getElementById('evaluation-form');
+const form2 = document.getElementById('form-data');
 botao.disabled = true;
+
+function botaoEnviar(event) {
+  event.preventDefault();
+  form1.style.display = 'none';
+  const nome = document.querySelector('#input-name').value;
+  const sobreNome = document.querySelector('#input-lastname').value;
+  const email1 = document.querySelector('#input-email').value;
+  const casa = document.querySelector('#house').value;
+  const textarea = document.querySelector('#textarea').value;
+  const obj = {
+    Nome: `${nome} ${sobreNome}`,
+    Email: email1,
+    Casa: casa,
+    Observações: textarea,
+  };
+  const novoElemento = document.createElement('p');
+  novoElemento.innerText = `Nome: ${obj.Nome}\nEmail: ${obj.Email}
+  Casa: ${obj.Casa}\nFamilia: ${obj.Familia}\nObservações: ${obj.Observações}`;
+  form2.appendChild(novoElemento);
+}
 
 function enviar(event) {
   event.preventDefault();
@@ -32,6 +54,7 @@ function contadorCaracteres() {
   document.getElementById('counter').innerText = quantidadeInicial;
 }
 
+botao.addEventListener('click', botaoEnviar);
 text.addEventListener('input', contadorCaracteres);
 chex.addEventListener('click', habilitar);
 btnEnviar.addEventListener('click', enviar);
